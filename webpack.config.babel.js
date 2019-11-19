41ae8e89c9a1c3c58d54fd078249b7c54ccff6e3
5onderling <lenny.anders.98@gmail.com>
2019-11-19 23:13:04 +0100

    updated webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,76 +2,130 @@
 
 import path from 'path';
 
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
-import VueLoaderPlugin from 'vue-loader/lib/plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import HtmlPlugin from 'html-webpack-plugin';
-import WebpackExtensionManifestPlugin from 'webpack-extension-manifest-plugin';
+import { CleanWebpackPlugin as Clean } from 'clean-webpack-plugin';
+import Copy from 'copy-webpack-plugin';
+import Vue from 'vue-loader/lib/plugin';
+import MiniCssExtract from 'mini-css-extract-plugin';
+import Html from 'html-webpack-plugin';
 
-import manifest from './src/manifest';
-import pkg from './package';
+import { version } from './package';
 
 const resolve = dir => path.resolve(__dirname, dir);
 
-module.exports = ({ NODE_ENV }) => ({
-  mode: NODE_ENV,
-  entry: {
-    background: './src/background/main.js',
-    content: './src/content/main.js',
-    newtab: './src/newtab/main.js'
-  },
-  output: {
-    filename: '[name]/main.js',
-    path: resolve('dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'resolve-url-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new CopyPlugin([{ from: 'public', to: resolve('dist'), force: true }]),
-    new WebpackExtensionManifestPlugin({
-      config: {
-        base: manifest,
-        extend: {
-          version: pkg.version,
-          content_security_policy:
-            NODE_ENV !== 'production'
-              ? (manifest.content_security_policy +=
-                  "script-src 'self' 'unsafe-eval'; object-src 'self'")
-              : manifest.content_security_policy
+module.exports = [
+  //
+  // background script + copy public files + generate manifest + clean
+  //
+  ({ NODE_ENV }) => ({
+    mode: NODE_ENV,
+    entry: './src/background/main.js',
+    output: {
+      filename: 'main.js',
+      path: resolve('dist/background')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
         }
-      }
-    }),
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({ filename: '[name]/main.css' }),
-    new HtmlPlugin({
-      chunks: ['newtab'],
-      template: './src/newtab/index.html',
-      filename: 'newtab/index.html'
-    })
-  ]
-});
+      ]
+    },
+    plugins: [
+      new Copy([
+        { from: 'public', to: resolve('dist'), force: true },
+        {
+          from: 'src/manifest.json',
+          to: resolve('dist/manifest.json'),
+          toType: 'file',
+          force: true,
+          transform(content) {
+            const manifest = JSON.parse(content.toString());
+            return JSON.stringify({
+              ...manifest,
+              version,
+              ...(NODE_ENV !== 'production' && {
+                content_security_policy: `${manifest.content_security_policy} script-src 'self' 'unsafe-eval'; object-src 'self'`
+              })
+            });
+          }
+        }
+      ]),
+      new Clean({
+        cleanOnceBeforeBuildPatterns: [resolve('dist/**/*')]
+      })
+    ]
+  }),
+  //
+  // content script
+  //
+  ({ NODE_ENV }) => ({
+    mode: NODE_ENV,
+    entry: './src/content/main.js',
+    output: {
+      filename: 'main.js',
+      path: resolve('dist/content')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader?shadowMode'
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ['vue-style-loader?shadowMode', 'css-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: ['vue-style-loader?shadowMode', 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    plugins: [new Vue(), new Clean()]
+  }),
+  //
+  // new tab page
+  //
+  ({ NODE_ENV }) => ({
+    mode: NODE_ENV,
+    entry: './src/newtab/main.js',
+    output: {
+      filename: 'main.js',
+      path: resolve('dist/newtab')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtract.loader, 'css-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtract.loader, 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    plugins: [
+      new Vue(),
+      new MiniCssExtract({ filename: 'main.css' }),
+      new Html({
+        template: './src/newtab/index.html',
+        filename: 'index.html'
+      }),
+      new Clean()
+    ]
+  })
+];
