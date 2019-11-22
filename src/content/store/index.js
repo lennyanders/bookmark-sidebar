0e20f8deef6136439286d93320cf25ad979e5b54
5onderling <lenny.anders.98@gmail.com>
2019-11-23 00:31:28 +0100

    added new store

---
+++ src/content/store/index.js
@@ -0,0 +1,67 @@
+import Vue from 'vue';
+
+export const store = Vue.observable({
+  barLeft: false,
+  barWidth: 320,
+
+  bm: {},
+  allFolders: [],
+
+  modalVisible: false,
+  modalType: '',
+  modalBm: {},
+
+  dragY: null,
+  dragEl: null,
+  newBmParentId: null
+});
+
+// uff
+export const getters = {
+  flattenedBms: () => {
+    let children = [];
+    JSON.stringify(store.bm.children, (_, nested) => {
+      if (nested && nested.title) children.push(nested);
+      return nested;
+    });
+    return children;
+  }
+};
+
+export const mutations = {
+  setBarLeft(barLeft) {
+    store.barLeft = barLeft;
+  },
+  setBarWidth(width) {
+    if (width < 280 || width > window.screen.width / 2) return;
+
+    store.barWidth = width;
+  },
+
+  setRootBm(bm) {
+    store.bm = bm;
+  },
+  setAllFolders(folders) {
+    store.allFolders = folders;
+  },
+
+  showModal(type, bm) {
+    store.modalVisible = true;
+    store.modalType = type;
+
+    if (bm) store.modalBm = bm;
+  },
+  hideModal() {
+    store.modalVisible = false;
+  },
+
+  setDragY(y) {
+    store.dragY = y;
+  },
+  setDragEl(el) {
+    store.dragEl = el;
+  },
+  setNewBmParentId(id) {
+    store.newBmParentId = id;
+  }
+};
