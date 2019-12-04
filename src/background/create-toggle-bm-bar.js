026c1edbbd142127b53ca49e3521e94e49b9ae36
5onderling <lenny.anders.98@gmail.com>
2019-12-04 14:17:49 +0100

    added keyboard shortcut (Strg+Shift+Y)

--- src/background/create-toggle-bm-bar.js
+++ src/background/create-toggle-bm-bar.js
@@ -1,31 +1,39 @@
 export let scriptRunsOnTab = new Set([]);
 
-chrome.browserAction.onClicked.addListener(tab => {
-  if (!tab.url.includes('chrome://newtab')) {
-    if (
-      [
-        'chrome://',
-        'https://chrome.google.com/webstore/',
-        'view-source:',
-        'file:///'
-      ].some(url => tab.url.includes(url))
-    ) {
-      chrome.tabs.create({
-        index: tab.index + 1,
-        url: 'chrome://newtab?bar=open'
-      });
-    } else {
-      if (scriptRunsOnTab.has(tab.id)) {
-        chrome.tabs.executeScript({
-          code: 'window.dispatchEvent(new CustomEvent("toggleBar"));'
-        });
-      } else {
-        chrome.tabs.insertCSS({ file: 'fonts/lato.css' });
-        chrome.tabs.executeScript({
-          file: 'content/main.js'
-        });
-        scriptRunsOnTab.add(tab.id);
-      }
-    }
+const createToggleBmBar = tab => {
+  tab = Array.isArray(tab) ? tab[0] : tab;
+
+  if (tab.url.includes('chrome://newtab')) return;
+
+  if (
+    [
+      'chrome://',
+      'https://chrome.google.com/webstore/',
+      'view-source:',
+      'file:///'
+    ].some(url => tab.url.includes(url))
+  ) {
+    return chrome.tabs.create({
+      index: tab.index + 1,
+      url: 'chrome://newtab?bar=open'
+    });
   }
+
+  if (scriptRunsOnTab.has(tab.id)) {
+    return chrome.tabs.executeScript({
+      code: 'window.dispatchEvent(new CustomEvent("toggleBar"));'
+    });
+  }
+
+  chrome.tabs.insertCSS({ file: 'fonts/lato.css' });
+  chrome.tabs.executeScript({ file: 'content/main.js' });
+  scriptRunsOnTab.add(tab.id);
+};
+
+chrome.commands.onCommand.addListener(command => {
+  if (command !== 'toogle-bm-bar') return;
+
+  chrome.tabs.query({ active: true, currentWindow: true }, createToggleBmBar);
 });
+
+chrome.browserAction.onClicked.addListener(createToggleBmBar);
