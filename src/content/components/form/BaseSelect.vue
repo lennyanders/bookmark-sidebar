6ea8235cdae000b5cbde4400c6f9ce1ef5db140a
5onderling <lenny.anders.98@gmail.com>
2020-09-12 23:30:40 +0200

    style select component

--- src/content/components/form/BaseSelect.vue
+++ src/content/components/form/BaseSelect.vue
@@ -47,6 +47,8 @@
 </script>
 
 <style lang="scss">
+  @import '../../functions.scss';
+
   .select {
     &__label {
       display: block;
@@ -54,7 +56,17 @@
     }
 
     &__el {
+      appearance: none;
+      width: 100%;
+      padding: 0.125em 0.25rem;
+      border: 2px solid var(--font-color);
       background-color: var(--bg-color);
+      background-image: svg-url(
+        '<svg viewBox="0 0 24 24"><path fill="#fff" d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42z"/></svg>'
+      );
+      background-repeat: no-repeat;
+      background-position: right 0.125em center;
+      background-size: 1.5em 1.5em;
     }
   }
 </style>
