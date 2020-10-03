6d842c3fc5c41452902cd8a6fd5046f58873639f
5onderling <lenny.anders.98@gmail.com>
2020-10-03 22:20:42 +0200

    refactor toggle logic on newtab - solves a problem where two tabs toggle at the same time

--- src/newtab/main.js
+++ src/newtab/main.js
@@ -6,20 +6,14 @@ import App from '../content/App';
 
 createApp(App).mount(sidebarEl);
 
-let tabId;
-chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
-  tabId = id;
-});
-const toggleBmBar = (tabOrTabs) => {
-  const tab = tabOrTabs[0] || tabOrTabs;
-  if (tab.id !== tabId) return;
+const toggleBmBar = () => {
+  if (!document.hasFocus()) return;
 
   window.dispatchEvent(new CustomEvent('toggleBar'));
 };
 
 chrome.commands.onCommand.addListener(async (command) => {
-  if (command !== 'toogle-bm-bar') return;
-  chrome.tabs.query({ active: true, currentWindow: true }, toggleBmBar);
+  if (command === 'toogle-bm-bar') toggleBmBar();
 });
 
 chrome.browserAction.onClicked.addListener(toggleBmBar);
