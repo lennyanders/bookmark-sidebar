4c35464e1d34f79a7e3cf9e904d48623d0a3bfdd
5onderling <lenny.anders.98@gmail.com>
2020-12-21 12:24:10 +0100

    remove imagesearch (use other extension instead)

--- src/newtab/App.vue
+++ src/newtab/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-  import TheFullScreenFileDrop from '@components/TheFullScreenFileDrop.vue';
   import TheBookmarkBar from '@sidebar/App.vue';
 </script>
 
 <template>
   <TheBookmarkBar />
-  <TheFullScreenFileDrop />
 </template>
 
 <style lang="scss">
